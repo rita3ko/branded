cda41d5cf36aec7ec16d79708279eb9b62551c33
Rita Kozlov <rita@cloudflare.com>
2024-04-15 11:37:31 -0400

    remove forreal

--- app/one-pager/route.tsx
+++ app/one-pager/route.tsx
@@ -1,16 +1,12 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { compile } from "@onedoc/react-print";
-import { Onedoc } from "@onedoc/client";
 import { OnePager } from "../documents/one-pager";
 import React from "react";
 
 export const maxDuration = 20;
 export const dynamic = "force-dynamic";
 export const runtime = 'edge';
-
-
-const onedoc = new Onedoc(process.env.ONEDOC_API_KEY!);
 
 export async function GET(req: NextRequest) {
   const nameData = JSON.parse(req.nextUrl.searchParams.get("nameData")!);
