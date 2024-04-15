f819744ca6f9976a754252fb4fe1cf1f2cf4f833
Rita Kozlov <rita@cloudflare.com>
2024-04-15 12:38:10 -0400

    fix not found again

--- app/_not-found/route.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server'
-export const runtime = 'edge';
-
-
-export async function GET(request: NextRequest) {
-  const redirectTo = request.nextUrl.clone()
-  redirectTo.pathname = '/error'
-  return NextResponse.redirect(redirectTo)
-}
